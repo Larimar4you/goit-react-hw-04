e69f48f670a3a7d3f1097a028acc372b228a7eb5
Larimar4you <Zajanezlaja@gmail.com>
2025-04-07 00:28:05 +0200

    g

--- src/components/Unsplash/Unsplash.js
+++ src/components/Unsplash/Unsplash.js
@@ -1,7 +1,7 @@
 // src/api/Unsplash.jsx
 import axios from 'axios';
 
-const ACCESS_KEY = 'mKkh-Y14DNYboULiiDnRKoej-5rAsS99EYQxCm4FxDAZ';
+const ACCESS_KEY = 'mKkh-Y14DNYboULiiDnRKoej-5rAsS99EYQxCm4FxDA';
 const BASE_URL = 'https://api.unsplash.com/search/photos';
 
 export const fetchImages = async (query, page = 1, perPage = 12) => {
